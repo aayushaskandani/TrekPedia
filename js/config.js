da9cba7660fecaf978a797e302628b598db583b7
Aayush <aayush@example.com>
2026-06-23 09:13:57 +0530

    Include config.js for Netlify deployment

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  SUPABASE_URL: "https://qipjlyrkbkkjowmfdwda.supabase.co", // Replace with your Supabase Project URL (e.g. https://xyz.supabase.co)
+  SUPABASE_ANON_KEY: "sb_publishable_RQPESmderp7i0gnJc4gjWw_P7mQbQgq", // Replace with your Supabase Anon Public Key
+  EMAILJS_PUBLIC_KEY: "wnVEDCYJQrHOeWpuI", // Replace with your EmailJS Public Key
+  EMAILJS_SERVICE_ID: "service_p9iadti", // Replace with your EmailJS Service ID
+  EMAILJS_TEMPLATE_ID: "template_dpfu0fh", // Replace with your EmailJS Template ID
+};
