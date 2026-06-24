e4c5a1aca2cb3cedc2e317f3faf1982f74b29e28
Aayush <aayush@example.com>
2026-06-24 10:19:25 +0530

    Add README, update gitignore

--- list_current_treks.js
+++
@@ -1,9 +0,0 @@
-import { TREKS } from './data/treks.js';
-
-console.log("Current treks in dataset:");
-Object.entries(TREKS).forEach(([country, list]) => {
-  console.log(`\n--- ${country} (${list.length} treks) ---`);
-  list.forEach(t => {
-    console.log(`  - [${t.id}] ${t.name} (${t.region})`);
-  });
-});
